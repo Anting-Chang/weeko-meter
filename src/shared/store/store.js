2baf9846e321c731ad4091dd9109c7381e7d440e
Anting Chang <caicainiaochang@gmail.com>
2021-06-12 04:36:33 -0400

    Updates

--- src/shared/store/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import colorReducer from "./slice/colorSlice";
-
-export default configureStore({
-    reducer: {
-        color: colorReducer
-    },
-})
